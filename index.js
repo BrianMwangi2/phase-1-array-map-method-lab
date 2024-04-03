38b59211aaed21a64894ce92e1d58bfdd14acfd5
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-03 12:49:38 +0300

    adds files

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    // Split the title into words
+    const words = title.split(' ');
+    // Capitalize the first letter of each word
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    // Join the capitalized words back into a single string
+    return capitalizedWords.join(' ');
+  });
 }
+
+console.log(titleCased());
